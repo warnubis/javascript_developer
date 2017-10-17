60ae1dcea5d9ca749e3f33b1578e06778d7617c3
warnubis <chris.reither@casc.at>
2017-10-17 03:03:37 +0200

    laufschrift

--- uebungen/laufschrift.1.js
+++ uebungen/laufschrift.1.js
@@ -1,27 +1,24 @@
 
 ;(function($) {
-$.fn.newsticker = function(  ) {
-//var settings = $.extend({
-  //color:'red'
-//},options);
-var text = " +++ Sport: Nachricht1 +++ Politik: Nachricht 2 +++ ";
+$.fn.newsticker = function() {
+
+
+
+return this.each(function() {
+  var text = $(this).val();
 var begin = 0;
 var end = text.length;
-
-return this.each( function() {
-  $('<newsticker>').val("" +
- text.substring(begin,end) + " " + text.substring(0,begin));
+console.log(text);
+  $(this).val( '' + text.substring( begin, end ) + ' ' + text.substring( 0, begin ) );
 
  begin ++;
- if(begin >= end)
- {
-  begin = 0;
- }
- //newsticker();
+ console.log(begin);
+ if(begin >= end) {begin = 0;}
+
+ setTimeout(newsticker, 50);
+
 
 });
-  //return this;
+
 };
 }(jQuery));
-// o12 = $.extend( {}, o1, o2); // zusammenlegen von o1 und o2 in einem neuem Objekt
-// $.extend( o1, o2); // zusammenlegen von o1 und o2 aber gespeichert wird in o1
